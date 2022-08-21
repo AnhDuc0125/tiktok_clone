4e16752bc7e5abddfe3dad04dffe8f8bf6841cac
anhduc0125 <vuducanh0125@gmail.com>
2022-08-21 21:57:00 +0700

    Thêm search

--- my-app/src/assets/images/index.js
+++ my-app/src/assets/images/index.js
@@ -1,5 +1,8 @@
 const images = {
   logo: require('~/assets/images/logo.svg').default,
+  search: require('~/assets/images/search.svg').default,
+  times: require('~/assets/images/times.svg').default,
+  loading: require('~/assets/images/loading.svg').default,
 };
 
 export default images;
--- my-app/src/components/Layouts/components/Header/index.js
+++ my-app/src/components/Layouts/components/Header/index.js
@@ -1,4 +1,6 @@
 import classNames from 'classnames/bind';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 
@@ -13,6 +15,21 @@ function Header() {
         <div className={cx('logo')}>
           <img src={images.logo} alt="Tiktok Logo" />
         </div>
+        <div className={cx('search')}>
+          <input
+            type="text"
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+          />
+          <button className={cx('clear')}>
+            <img src={images.times} alt="" />
+          </button>
+          <img className={cx('spinner')} src={images.loading} alt="" />
+          <button className={cx('search-btn')}>
+            <img src={images.search} alt="Search" />
+          </button>
+        </div>
+        <div className={cx('action')}></div>
       </div>
     </header>
   );
